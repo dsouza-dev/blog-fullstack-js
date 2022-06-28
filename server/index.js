84268cc825ed53c210c35c05f097e1efedd4753e
Daniel Souza <workdndsza@gmail.com>
2022-06-28 12:11:58 -0300

    criando as rotas de ler os posts e criar-los

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-
 const db = require('./models')
+
+app.use(express.json())
+
+// Routers
+const postRouter = require('./routes/Posts')
+app.use("/posts", postRouter)
 
 db.sequelize.sync().then(() => {
   console.log("Database is synced right now")
