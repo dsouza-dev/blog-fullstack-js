db07f37367f060affcaeae9e97d0b5cb7637d214
daniel souza <workdndsza@gmail.com>
2022-07-01 07:38:04 -0300

    tela inicial react

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+    const [listPost, setListPost] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:3001/posts')
+            .then(
+                (response) => {
+                    console.log(response.data)
+                    setListPost(response.data)
+                }
+            )
+    }, [])
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Oi.
-                </p>
-            </header>
+        <div className='App'>
+            {listPost.map((value, key) => {
+                return (
+                    <div className='post'>
+                        <div className='title'> Titulo: {value.title} </div>
+                        <div className='body'> Texto: {value.postText} </div>
+                        <div className='footer'> Autor: {value.username}  </div>
+                        <hr />
+                    </div>
+                )
+            })}
         </div>
-    );
+    )
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const db = require('./models')
 
+app.use(cors())
 app.use(express.json())
 
 // Routers
@@ -9,8 +11,8 @@ const postRouter = require('./routes/Posts')
 app.use("/posts", postRouter)
 
 db.sequelize.sync().then(() => {
-  console.log("Database is synced right now")
-  app.listen(3001, () => {
-    console.log('The server is running in the port 3001')
-  })
+    console.log("Database is synced right now")
+    app.listen(3001, () => {
+        console.log('The server is running in the port 3001')
+    })
 })
